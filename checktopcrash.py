0c8e7a63b0c2ef4126fbd508390f95624fbc4155
yglazko <yglazko@yglazko.local>
2015-06-10 14:15:25 -0700

    Added support for top crashes from multiple versions of 39b1

--- checktopcrash.py
+++ checktopcrash.py
@@ -46,10 +46,58 @@ for k in json_string2['crashes']:
 			if x == tempSig:
 				l.topCrash.append(str(k["currentRank"] + 1))
 
+url3 = 'https://crash-stats.mozilla.com/api/TCBS/?product=Firefox&version=39.0b1&crash_type=Browser'
+crash_results3 = requests.get(url3)
+json_string3 = json.loads(crash_results3.text)
+
+for k in json_string3['crashes']:
+	tempSig = str(k["signature"])
+	#print tempSig
+	for l in bugList:
+		for x in l.sigList:
+			if x == tempSig:
+				l.topCrash.append(str(k["currentRank"] + 1))
+
+url4 = 'https://crash-stats.mozilla.com/api/TCBS/?product=Firefox&version=39.0b2&crash_type=Browser'
+crash_results4 = requests.get(url4)
+json_string4 = json.loads(crash_results4.text)
+
+for k in json_string4['crashes']:
+	tempSig = str(k["signature"])
+	#print tempSig
+	for l in bugList:
+		for x in l.sigList:
+			if x == tempSig:
+				l.topCrash.append(str(k["currentRank"] + 1))
+
+url5 = 'https://crash-stats.mozilla.com/api/TCBS/?product=Firefox&version=39.0b3&crash_type=Browser'
+crash_results5 = requests.get(url5)
+json_string5 = json.loads(crash_results5.text)
+
+for k in json_string5['crashes']:
+	tempSig = str(k["signature"])
+	#print tempSig
+	for l in bugList:
+		for x in l.sigList:
+			if x == tempSig:
+				l.topCrash.append(str(k["currentRank"] + 1))
+
+url6 = 'https://crash-stats.mozilla.com/api/TCBS/?product=Firefox&version=39.0b4&crash_type=Browser'
+crash_results6 = requests.get(url6)
+json_string6 = json.loads(crash_results6.text)
+
+for k in json_string6['crashes']:
+	tempSig = str(k["signature"])
+	#print tempSig
+	for l in bugList:
+		for x in l.sigList:
+			if x == tempSig:
+				l.topCrash.append(str(k["currentRank"] + 1))
+
 
 
 for bugs in bugList:
 	if len(bugs.topCrash) > 0:
-		print "Bug " + bugs.iD + " has " + str(len(bugs.sigList)) + " crash signatures, and their ranks are: ",
+		print "Bug " + bugs.iD + " has " + str(len(bugs.sigList)) + " crash signatures, and their ranks are: "
 		for crashes in bugs.topCrash:
 			print crashes
